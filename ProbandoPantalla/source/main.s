27ad44f44355feb0dc1f42cf6d86ea1fd6dd0d09
danielapa <Daniela@Danielas-MacBook-Pro.local>
2015-11-16 21:31:06 -0600

    cambio

--- ProbandoPantalla/source/main.s
+++ ProbandoPantalla/source/main.s
@@ -16,7 +16,7 @@
 * to the elf file. Convention dictates that the symbol _start is used for the 
 * entry point, so this all has the net effect of setting the entry point here.
 * Ultimately, this is useless as the elf itself is not used in the final 
-* result, and so the entry point really doesn't matter, but it aids clarity,
+* result, and so the entry point really doesnt matter, but it aids clarity,
 * allows simulators to run the elf, and also stops us getting a linker warning
 * about having no entry point. 
 */
