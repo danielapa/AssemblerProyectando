9c2e05eb7512d9a5853a4403b3bc8b60f2cfab0c
Belen <mariabelenhb@gmail.com>
2015-11-24 02:16:43 -0600

    Probando

--- animacionDrawPixelModificado/source/boss.s
+++ animacionDrawPixelModificado/source/boss.s
@@ -50,7 +50,7 @@ jugarBoss:
         bl drawImageWithTransparency2
         // -----------------------------
 
-<<<<<<< HEAD
+
         /*ldr r0, =Fly1Height
         mov r1, xfly
         mov r2, yfly
@@ -77,13 +77,12 @@ jugarBoss:
         cmp enemigo, #2
         ldreq dirEnemigoH, =Slime1Height
         ldreq dirEnemigoW, =Slime1Width
-=======
 
         ldr r7, =ContadorPoderes
         ldr r7, [r7]
         cmp r7, #5
         blgt AnimandoPoderes
->>>>>>> origin/master
+
 
         // *******************************
         // 1 - revisar teclas
